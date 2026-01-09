d87b3c36fb8d83d425a8697e5eebd363a53da4d0
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2026-01-09 12:53:30 +0700

    menambahkan sealed interface untuk status UI detail

--- app/src/main/java/com/example/questfirebase_187/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase_187/viewmodel/DetailViewModel.kt
@@ -1,4 +1,19 @@
 package com.example.questfirebase_187.viewmodel
 
-class DetailViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questfirebase_187.modeldata.Siswa
+import com.example.questfirebase_187.repositori.RepositorySiswa
+import com.example.questfirebase_187.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val siswa: Siswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
 }
